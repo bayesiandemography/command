c53b9e5cf356f90412656016fc0784d0dadb65e6
John Bryant <john.r.bryant@gmail.com>
2022-12-16 19:57:14 +0700

    started vignette

--- inst/extdata/swiss_fertility/run_all.sh
+++ inst/extdata/swiss_fertility/run_all.sh
@@ -1,37 +1,20 @@
 
-## Prepare data
-## (create file 'out/dataset.rds')
-
+## Read in data, and output to 'out/dataset.rds'
 Rscript src/dataset.R data/swiss.csv
 
-
-## Run model
-## (create file 'out/model.rds')
-
+## Run model, and output results to 'out/model.rds'
 Rscript src/model.R out/dataset.rds --robust_method=MM
 
-
-## Output coefficient estimates
-## (create file 'out/tab_coef.csv')
-
+## Put estimates in 'out/tab_coef.csv'
 Rscript src/tab_coef.R out/model.rds
 
-
-## Extract actual and predicted fertility
-## (create file 'out/vals_fertility.rds')
-
+## Put values for fertility in 'out/vals_fertility.rds'
 Rscript src/vals_fertility.R out/model.rds
 
-
-## Graph actual and predicted fertility
-## (create file 'out/fig_fertility.pdf')
-
+## Put graph of fertility in 'out/fig_fertility.pdf'
 Rscript src/fig_fertility.R out/vals_fertility.rds --robust_method=MM
 
-
-## Output numbers for actual and predicted fertility
-## (create file 'out/tab_fertility.csv')
-
+## Put data from graph in 'out/tab_fertility.csv'
 Rscript src/tab_fertility.R out/vals_fertility.rds --digits_round=1
 
 
