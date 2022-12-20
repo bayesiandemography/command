48ab9afb7d633e91b12099eb939e42c0cd7872b0
John Bryant <john.r.bryant@gmail.com>
2022-12-20 08:18:47 +0700

    revised help

--- R/cmd_assign.R
+++ R/cmd_assign.R
@@ -1,18 +1,33 @@
 
 #' Process command line arguments
 #'
+#' @description 
 #' Create objects in the current environment,
 #' based on arguments passed at the command line.
+#' For instance, if a script `tidy_data.R` containing the
+#' lines
+#' 
+#' `cmd_assign(raw_data = "raw_data.rds",
+#'             unit = "kg")`
+#'             
+#' is run at the command line using
+#' 
+#' `Rscript tidy_data.R raw_data.rds --unit=kg`
+#' 
+#' then objects called `raw_data` and `unit` are
+#' created in the current environment.
 #'
+#' @details
 #' # Types of session
 #'
-#' `cmd_assign()` behaves different depending on how it is called:
+#' `cmd_assign()` behaves differently depending
+#'  on how it is called:
 #'
-#' * If `cmd_assign()` is called in a script that is
-#'   being run from the command line, then it uses
-#'   values that were passed at the command line.
+#' * If `cmd_assign()` is called inside a script
+#'   being run from the command line, it processes
+#'   values passed at the command line.
 #' * If `cmd_assign()` is called in an interactive
-#'   session, then it uses default values.
+#'   session, it uses the name-value pairs from the call.
 #' 
 #' `cmd_assign()` is typically used interactively
 #' when developing or testing code, and in command
@@ -20,33 +35,20 @@
 #'
 #' # Matching names and values
 #'
-#' `cmd_assign()` starts by processing named arguments.
-#' `cmd_assign()` treats an argument as named
-#' if the argument has the form
+#' When used in command line mode, `cmd_assign()`
+#' first processes named command-line arguments,
+#' and then processes unnamed command-line arguments.
 #' 
-#' `-<single-letter>=<value>`
+#' `cmd_assign()` treats a command-line argument as named
+#' if the argument has the form `-<single-letter>=<value>`,
+#' eg `-n=100`, or `--<name>=<value>`, 
+#' eg `--n_iteration=100`. (Note that there are no 
+#' spaces around the equals signs.)
 #'
-#' or
-#'
-#' `--<name>=<value>`
-#'
-#' for instance
+#' `cmd_assign()` matches unnamed command line arguments
+#' in the order in which they appear.
 #' 
-#' `-n=100`
-#' 
-#' or
-#' 
-#' `--n_iteration=100`
-#' 
-#' (Note that there are no spaces around the 
-#' equals signs.)
-#'
-#' Once `cmd_assign()` has processed named 
-#' command line arguments, it matches unnamed
-#' arguments to unused names in the call to
-#' `cmd_assign()`.
-#' 
-#' Consider, for instance, the script `analysis.R`
+#' If, for instance, the script `analysis.R`
 #' containing the lines
 #' 
 #' ```R
@@ -55,41 +57,41 @@
 #'            max_age = 85)
 #' ```
 #' 
-#' If `analysis.R` is launched from the
-#' command line using
+#' is launched from the command line using
 #' ```R
 #' Rscript analysis.R --max_age=90 person.csv --impute=TRUE
 #' ```
-#' then `cmd_assign()` will proceed as follows
-#' - look for a named command line argument `impute`,
-#'   and, in R, assign the name `impute` to whatever
-#'   value was passed (`"TRUE"` in this case)
-#' - look for a named command line argument `max_age`,
-#'   and, in R, assign the name `max_age` to whatever
-#'   value was passed (`"90"` in this case)
-#' - look for an unnamed command line argument,
-#'   and, in R, assign the name `data` to whatever
-#'   value was passed (`"person.csv"` in this case).
-#' 
-#' `cmd_assign()` only reverts to defaults when 
-#' it is in interactive mode. When it is
-#' in command line mode, every 
-#'  `cmd_assign()`
-#' must be supplied with a value at the command line.
+#' then `cmd_assign()` proceeds as follows:
+#' 1. Named command-line arguments
+#'     - `--max_age=90`. Look in the call to `cmd_assign()` 
+#'      for an argument called `max_age`. If there is  one,
+#'      create an object called `max_age` holding the value
+#'      `90`. If not, raise an error.
+#'    - `--impute=TRUE`. Look in the call to `cmd_assign()` 
+#'      for an argument called `impute`. If there is one,
+#'      create an object called `impute` holding the value
+#'      `TRUE`. If not, raise an error.
+#' 2. Unnamed command-line arguments
+#'     - `person.csv`. Count the number of unused name-value
+#'        pairs from the call to `cmd_assign()`. If there is
+#'        exactly one, create an object with that name
+#'        holding the value `"person.csv"`. If not, 
+#'        raise an error.
+#'        
 #'
 #' # Creating objects
 #'
 #' The way that an object is created in the
 #' current R environment depends on the
 #' type of value passed at the command line
-#' and/or the value supplied as a default.
+#' and sometimes on the value supplied as a default.
 #'
 #' ## Value looks like a `.rds` file path
 #'
 #' If the value looks like the file path
 #' for an `.rds` object (ie an object created
 #' by a function  such as [base::saveRDS]),
-#' then `cmd_assign()` tries to load the file.
+#' then `cmd_assign()` tries to load the `.rds` file.
 #' For instance, in an interactive session,
 #' 
 #' `cmd_assign(costs = "costs.rds")`
@@ -99,8 +101,8 @@
 #' `costs <- readRDS("costs.rds")`
 #'
 #' To avoid automatically loading an `.rds`
-#' file, give the argument
-#' a name starting with a dot. For instance,
+#' file, give the argument a name  that starts
+#' with a dot. For instance,
 #' in an interactive session,
 #'
 #' `cmd_assign(.costs = "costs.rds")`
@@ -134,25 +136,26 @@
 #' ## Everything else
 #'
 #' Otherwise, arguments passed at the command line
-#' are simply copied into the current environment,
+#' are copied into the current environment,
 #' as named strings.
-#'
-#' # More information
-#'
-#' The vignette `vignette("command")`
-#' gives examples of the use of `cmd_assign()`,
-#' and discusses how `cmd_assign` can be used in the
-#' workflow for a data analysis.
 #'
 #' @param ... Name-value pairs.
 #' 
-#' @returns `cmd_assign()` is normally called
+#' @returns `cmd_assign()` is called
 #' for its side effect, which is to create objects
 #' in the current environment. However, `cmd_assign()`
 #' also invisibly returns a named list of objects.
 #'
-#' @seealso Function [utils::Rscript()] and
-#' package `littler` can be used to run
+#' @seealso 
+#' The vignette `vignette("command")`
+#' gives examples of the use of `cmd_assign()`,
+#' and discusses how `cmd_assign` can be used in the
+#' workflow for a data analysis.#' 
+#' 
+#' Function [utils::Rscript()] and
+#' package 
+#' [littler](https://CRAN.R-project.org/package=littler)
+#' can be used to run R
 #' scripts from the command line.
 #' 
 #' Internally, `cmd_assign()` uses
