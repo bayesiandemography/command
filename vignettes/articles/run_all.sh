d769c993b1a96f9e631521621fd1ba287352ab1e
John Bryant <john.r.bryant@gmail.com>
2025-04-01 19:56:07 +0700

    tidied articles folder

--- vignettes/articles/run_all.sh
+++
@@ -1,9 +0,0 @@
-
-Rscript src/fig_smoothed.R data/airmiles.csv 5 out/fig_smoothed_5.png 
-
-Rscript src/fig_smoothed.R data/airmiles.csv 10 out/fig_smoothed_10.png 
-
-R --quiet -e "rmarkdown::render('report_smooth.Rmd')"
-
-
-
