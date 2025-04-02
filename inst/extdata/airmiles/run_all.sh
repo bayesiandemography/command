de999eb3ffe4caafe5385f4a376e56b52c871f9c
John Bryant <john.r.bryant@gmail.com>
2025-04-02 07:15:51 +0700

    removed folders from airmiles example

--- inst/extdata/airmiles/run_all.sh
+++ inst/extdata/airmiles/run_all.sh
@@ -1,7 +1,7 @@
 
-Rscript src/fig_smoothed.R data/airmiles.csv 5 out/fig_smoothed_5.png 
+Rscript fig_smoothed.R airmiles.csv 5 fig_smoothed_5.png 
 
-Rscript src/fig_smoothed.R data/airmiles.csv 10 out/fig_smoothed_10.png 
+Rscript fig_smoothed.R airmiles.csv 10 fig_smoothed_10.png 
 
 R --quiet -e "rmarkdown::render('report_smooth.Rmd')"
 
